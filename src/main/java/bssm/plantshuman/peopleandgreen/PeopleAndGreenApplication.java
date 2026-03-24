3af6415f271b4fee4fc0a49d014e507a0711fa99
leegunwoo <leegunwoo0325@gmail.com>
2026-03-24 10:28:28 +0900

    initial commit

---
+++ src/main/java/bssm/plantshuman/peopleandgreen/PeopleAndGreenApplication.java
@@ -0,0 +1,13 @@
+package bssm.plantshuman.peopleandgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleAndGreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleAndGreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bssm/plantshuman/peopleandgreen/PeopleAndGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.plantshuman.peopleandgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleAndGreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
