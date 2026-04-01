02a918dd18d5843934b62de9c091431da97984d8
jaeminjo732 <jaeminjo732@gmail.com>
2026-04-01 14:35:36 +0900

    fix(#8): CI 테스트 데이터소스를 H2로 고정

--- src/test/java/bssm/plantshuman/peopleandgreen/recommendation/adapter/out/persistence/RecommendationCatalogPersistenceIntegrationTest.java
+++ src/test/java/bssm/plantshuman/peopleandgreen/recommendation/adapter/out/persistence/RecommendationCatalogPersistenceIntegrationTest.java
@@ -10,7 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class RecommendationCatalogPersistenceIntegrationTest {
 
     @Autowired
